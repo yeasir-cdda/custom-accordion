755e1742fa24740e9654b59a629316623afd4d71
Yeasir <yeasir@cdda.io>
2022-07-04 23:52:04 +0600

    design done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Box } from "@chakra-ui/react";
 import CustomAccordion from "./components/CustomAccordion";
-
+import './font.css'
 function App() {
   return (
     <div className="App">
-      <CustomAccordion/>
+      <Box w={'820px'} mx='auto' mt='20'>
+      <CustomAccordion  allowMultiple allowToggle/>
+      </Box>
     </div>
   );
 }
--- src/components/CustomAccordion.js
+++ src/components/CustomAccordion.js
@@ -1,48 +1,72 @@
 import React from 'react';
+import { RiArrowDownSLine, } from "react-icons/ri";
 import {
     Accordion,
     AccordionItem,
     AccordionButton,
     AccordionPanel,
-    AccordionIcon,
-    Box
+    Box,
+
 } from '@chakra-ui/react'
-const CustomAccordion = () => {
+const CustomAccordion = ({ allowMultiple, allowToggle }) => {
     return (
-        <Accordion defaultIndex={[0]} allowMultiple >
+        <Accordion border='1px' borderColor={'#EBEBEB'} defaultIndex={[0]} allowMultiple={allowMultiple ? true : false} allowToggle={allowToggle ? true : false} >
             <AccordionItem>
-                <h2>
-                    <AccordionButton>
-                        <Box flex='1' textAlign='left'>
-                            Section 1 title
-                        </Box>
-                        <AccordionIcon />
-                    </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat.
-                </AccordionPanel>
+                {({ isExpanded }) => (
+                    <>
+                        <h2>
+                            <AccordionButton _expanded={{ bg: '#F7F7F7', borderLeft: '3px solid', color: '#23A6F0' }} >
+                                <Box ml='28px' py='22px' fontSize={'16px'} fontWeight='700' fontFamily='montserrat' flex='1' textAlign='left'>
+                                    Section 1 title
+                                </Box>
+                                {isExpanded ? <Box bg='#23A6F0' color={'white'} borderRadius={'30px'} boxShadow='0px 2px 4px rgba(0, 0, 0, 0.1)' w='32px' h='32px' position={'relative'} mr='28px'>
+                                    <Box fontSize={'20px'} transform='rotate(-180deg)' transition={'ease-in-out .25s'} position='absolute' top={'6px'} left={'6px'}>
+                                        <RiArrowDownSLine />
+                                    </Box> </Box> : <Box border={'1px'} borderColor='#D3D3D3' borderRadius={'30px'} boxShadow='0px 2px 4px rgba(0, 0, 0, 0.1)' w='32px' h='32px' position={'relative'} mr='28px'>
+                                    <Box fontSize={'20px'} transform='rotate(-90deg)' color={'black'} transition={'ease-in-out .25s'} position='absolute' top={'5px'} left={'5px'}>
+                                        <RiArrowDownSLine />
+                                    </Box>
+                                </Box>}
+                            </AccordionButton>
+                        </h2>
+                        <AccordionPanel backgroundColor={'#F7F7F7'} fontSize={'16px'} fontFamily='montserrat' fontWeight={'500'} px='38px' pb={4}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            commodo consequat.
+                        </AccordionPanel>
+                    </>
+                )}
+            </AccordionItem>
+            <AccordionItem>
+                {({ isExpanded }) => (
+                    <>
+                        <h2>
+                            <AccordionButton _expanded={{ bg: '#F7F7F7', borderLeft: '3px solid', color: '#23A6F0' }} >
+                                <Box ml='28px' py='22px' fontSize={'16px'} fontWeight='700' fontFamily='montserrat' flex='1' textAlign='left'>
+                                    Section 2 title
+                                </Box>
+                                {isExpanded ? <Box bg='#23A6F0' color={'white'} borderRadius={'30px'} boxShadow='0px 2px 4px rgba(0, 0, 0, 0.1)' w='32px' h='32px' position={'relative'} mr='28px'>
+                                    <Box fontSize={'20px'} transform='rotate(-180deg)' transition={'ease-in-out .25s'} position='absolute' top={'6px'} left={'6px'}>
+                                        <RiArrowDownSLine />
+                                    </Box> </Box> : <Box border={'1px'} borderColor='#D3D3D3' borderRadius={'30px'} boxShadow='0px 2px 4px rgba(0, 0, 0, 0.1)' w='32px' h='32px' position={'relative'} mr='28px'>
+                                    <Box fontSize={'20px'} transform='rotate(-90deg)' color={'black'} transition={'ease-in-out .25s'} position='absolute' top={'5px'} left={'5px'}>
+                                        <RiArrowDownSLine />
+                                    </Box>
+                                </Box>}
+                            </AccordionButton>
+                        </h2>
+                        <AccordionPanel backgroundColor={'#F7F7F7'} fontSize={'16px'} fontFamily='montserrat' fontWeight={'500'} px='38px' pb={4}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                            commodo consequat.
+                        </AccordionPanel>
+                    </>
+                )}
             </AccordionItem>
 
-            <AccordionItem>
-                <h2>
-                    <AccordionButton>
-                        <Box flex='1' textAlign='left'>
-                            Section 2 title
-                        </Box>
-                        <AccordionIcon />
-                    </AccordionButton>
-                </h2>
-                <AccordionPanel pb={4}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                    commodo consequat.
-                </AccordionPanel>
-            </AccordionItem>
+
         </Accordion>
     );
 };
